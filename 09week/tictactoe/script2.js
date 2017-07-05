5d3b6d932a45082a2d8bd248976e71f43cfbbc5e
meganroone <emailgena@gmail.com>
2017-07-05 17:04:23 -0500

    portofino update

--- 09week/tictactoe/script2.js
+++ 09week/tictactoe/script2.js
@@ -1,90 +1,209 @@
-$(
-  function() {
-
-  let playerTurn = 'X';
-  $('#current-turn').text(playerTurn);
+$(function() {
+  let player = 'X';
 
   $('[data-cell]').click(function() {
-    $(this).text(playerTurn);
-    if (playerTurn === 'X') {
-      checkWin();
-      playerTurn = 'O';
-      $('#current-turn').text(playerTurn);
+    $(this).text(player);
+    if(checkWin()) {
+      $('#announce-winner').text(`${player} wins!`)
     } else {
-      checkWin();
-      playerTurn = 'X';
-      $('#current-turn').text(playerTurn);
+      if (player === 'X') {
+        player = 'O';
+      }else {
+        player = 'X';
+      }
     }
+
   })
-  $('#clear').click(function() {
-    $('[data-cell]').text(null);
-    $('#announce-winner').text(null);
-  })
+
   function checkWin() {
     if (
-      $('[data-cell="0"]').text() === playerTurn &&
-      $('[data-cell="1"]').text() === playerTurn &&
-      $('[data-cell="2"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="3"]').text() === playerTurn &&
-      $('[data-cell="4"]').text() === playerTurn &&
-      $('[data-cell="5"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="6"]').text() === playerTurn &&
-      $('[data-cell="7"]').text() === playerTurn &&
-      $('[data-cell="8"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="0"]').text() === playerTurn &&
-      $('[data-cell="3"]').text() === playerTurn &&
-      $('[data-cell="6"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="1"]').text() === playerTurn &&
-      $('[data-cell="4"]').text() === playerTurn &&
-      $('[data-cell="7"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="2"]').text() === playerTurn &&
-      $('[data-cell="5"]').text() === playerTurn &&
-      $('[data-cell="8"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="0"]').text() === playerTurn &&
-      $('[data-cell="4"]').text() === playerTurn &&
-      $('[data-cell="8"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else if (
-      $('[data-cell="2"]').text() === playerTurn &&
-      $('[data-cell="4"]').text() === playerTurn &&
-      $('[data-cell="6"]').text() === playerTurn
-    )
-      {
-        $('#announce-winner').text(playerTurn + ' wins!');
-      }
-    else {}
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="3"]').text() === 'X' &&
+      $('[data-cell="6"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="3"]').text() === 'O' &&
+      $('[data-cell="6"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="0"]').text() === 'X' &&
+      $('[data-cell="1"]').text() === 'X' &&
+      $('[data-cell="2"]').text() === 'X' ||
+      $('[data-cell="0"]').text() === 'O' &&
+      $('[data-cell="1"]').text() === 'O' &&
+      $('[data-cell="2"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="1"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="7"]').text() === 'X' ||
+      $('[data-cell="1"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="7"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="2"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="6"]').text() === 'X' ||
+      $('[data-cell="2"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="6"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="2"]').text() === 'X' &&
+      $('[data-cell="5"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="2"]').text() === 'O' &&
+      $('[data-cell="5"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="3"]').text() === 'X' &&
+      $('[data-cell="4"]').text() === 'X' &&
+      $('[data-cell="5"]').text() === 'X' ||
+      $('[data-cell="3"]').text() === 'O' &&
+      $('[data-cell="4"]').text() === 'O' &&
+      $('[data-cell="5"]').text() === 'O'
+    ) {
+      return true;
+    } else if(
+      $('[data-cell="6"]').text() === 'X' &&
+      $('[data-cell="7"]').text() === 'X' &&
+      $('[data-cell="8"]').text() === 'X' ||
+      $('[data-cell="6"]').text() === 'O' &&
+      $('[data-cell="7"]').text() === 'O' &&
+      $('[data-cell="8"]').text() === 'O'
+    ) {
+      return true;
+    }else {
+      return false;
     }
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+// $(
+//   function() {
+//
+//   let playerTurn = 'X';
+//   $('#current-turn').text(playerTurn);
+//
+//   $('[data-cell]').click(function() {
+//     $(this).text(playerTurn);
+//     if (playerTurn === 'X') {
+//       checkWin();
+//       playerTurn = 'O';
+//       $('#current-turn').text(playerTurn);
+//     } else {
+//       checkWin();
+//       playerTurn = 'X';
+//       $('#current-turn').text(playerTurn);
+//     }
+//   })
+//   $('#clear').click(function() {
+//     $('[data-cell]').text(null);
+//     $('#announce-winner').text(null);
+//   })
+//   function checkWin() {
+//     if (
+//       $('[data-cell="0"]').text() === playerTurn &&
+//       $('[data-cell="1"]').text() === playerTurn &&
+//       $('[data-cell="2"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="3"]').text() === playerTurn &&
+//       $('[data-cell="4"]').text() === playerTurn &&
+//       $('[data-cell="5"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="6"]').text() === playerTurn &&
+//       $('[data-cell="7"]').text() === playerTurn &&
+//       $('[data-cell="8"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="0"]').text() === playerTurn &&
+//       $('[data-cell="3"]').text() === playerTurn &&
+//       $('[data-cell="6"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="1"]').text() === playerTurn &&
+//       $('[data-cell="4"]').text() === playerTurn &&
+//       $('[data-cell="7"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="2"]').text() === playerTurn &&
+//       $('[data-cell="5"]').text() === playerTurn &&
+//       $('[data-cell="8"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="0"]').text() === playerTurn &&
+//       $('[data-cell="4"]').text() === playerTurn &&
+//       $('[data-cell="8"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else if (
+//       $('[data-cell="2"]').text() === playerTurn &&
+//       $('[data-cell="4"]').text() === playerTurn &&
+//       $('[data-cell="6"]').text() === playerTurn
+//     )
+//       {
+//         $('#announce-winner').text(playerTurn + ' wins!');
+//       }
+//     else {}
+//     }
+// })
---
+++ forfun/react/reactstuff.js
@@ -0,0 +1,83 @@
+// class ShoppingList extends React.Component {
+//   render() {
+//     return (
+//       <div className="shopping-list">
+//       <h1> Shopping List for {this.props.name}</h1>
+//       <ul>
+//         <li>Instagram</li>
+//         <li>WhatsApp</li>
+//         <li>Oculus</li>
+//       </ul>
+//     </div>
+//   );
+//   }
+// }
+// return React.createElement('div', {className: 'shopping-list'},
+//   React.createElement('h1', /* ... h1 children ... */),
+//   React.createElement('ul', /* ... ul children ... */)
+// );
+
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
